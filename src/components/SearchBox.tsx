8c13c16ac0cce47c449cb4c9eb103393419889cb
SMRSAN <57676740+smrsan@users.noreply.github.com>
2025-02-07 19:24:07 +0330

    Make SearchBox sticky

--- src/components/SearchBox.tsx
+++ src/components/SearchBox.tsx
@@ -6,7 +6,7 @@ interface SearchBoxProps {
 
 const SearchBox = ({ onChange }: SearchBoxProps) => {
   return (
-    <div className="mx-auto mb-4 flex items-center justify-center">
+    <div className="sticky top-0 z-10 py-2 bg-white mx-auto mb-4 flex items-center justify-center">
       <div className="flex flex-row border border-gray-400 w-full rounded-md overflow-hidden">
         <input
           type="text"
