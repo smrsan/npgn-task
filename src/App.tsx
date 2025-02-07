ad048b703cf44acc60a352381c17aa6064d07296
SMRSAN <57676740+smrsan@users.noreply.github.com>
2025-02-07 19:17:11 +0330

    Implement & design employee comparison feature

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,12 @@ function App() {
   const [isLoading, setIsLoading] = useState(false);
   const [employeeList, setEmployeeList] = useState<IEmployeeData[]>([]);
   const [filteredList, setFilteredList] = useState<IEmployeeData[]>([]);
+  const [comparisons, setComparisons] = useState<Record<number, IEmployeeData>>(
+    {}
+  );
+  const comparisonsEntries = Object.entries(comparisons);
+  const comparisonsCount = comparisonsEntries.length;
+  const canCompare = comparisonsCount < 4;
   const isMountedRef = useIsMounted();
 
   const fetchEmployeesData = useCallback(async () => {
@@ -48,6 +54,21 @@ function App() {
     [employeeList]
   );
 
+  const handleToggleCompare = useCallback(
+    (emp: IEmployeeData) => {
+      if (emp.id in comparisons) {
+        setComparisons((prev) => {
+          const newComparisons = { ...prev };
+          delete newComparisons[emp.id];
+          return newComparisons;
+        });
+      } else {
+        setComparisons((prev) => ({ ...prev, [emp.id]: emp }));
+      }
+    },
+    [comparisons]
+  );
+
   useEffect(() => {
     fetchEmployeesData();
     // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -59,8 +80,63 @@ function App() {
       <CardList>
         {isLoading
           ? Array.from({ length: 10 }).map((_, i) => <CardSkeleton key={i} />)
-          : filteredList.map((emp) => <Card key={emp.id} employee={emp} />)}
+          : filteredList.map((emp) => (
+              <Card
+                key={emp.id}
+                employee={emp}
+                onToggleCompare={handleToggleCompare}
+                isCompared={emp.id in comparisons}
+                canCompare={canCompare}
+              />
+            ))}
       </CardList>
+      <div
+        className={`sticky bottom-0 w-full mt-2 shadow border-t-amber-400 border-t-4 grid ${
+          {
+            2: "grid-cols-2",
+            3: "grid-cols-3",
+            4: "grid-cols-4",
+            5: "grid-cols-5",
+          }[comparisonsCount + 1]
+        } bg-gray-200 transition ${
+          comparisonsCount > 1 ? "opacity-100" : "opacity-0 pointer-events-none"
+        }`}
+      >
+        <div className="p-2 bg-white"></div>
+        {comparisonsEntries.map(([id, emp]) => (
+          <div key={id} className="p-2 text-center bg-white font-bold">
+            {emp.employee_name.split(" ")[0]}
+            <br />
+            {emp.employee_name.split(" ")[1]}
+            <br />
+          </div>
+        ))}
+        <div
+          className={`h-[1px] ${
+            {
+              2: "col-span-2",
+              3: "col-span-3",
+              4: "col-span-4",
+              5: "col-span-5",
+            }[comparisonsCount + 1]
+          }`}
+        />
+
+        <div className="p-2 bg-gray-100 text-left">Salary</div>
+        {comparisonsEntries.map(([id, emp]) => (
+          <div key={id} className="p-2 bg-white text-center">
+            ${emp.employee_salary.toLocaleString()}
+          </div>
+        ))}
+        <div className={`h-[1px] col-span-${comparisonsCount + 1}`} />
+
+        <div className="p-2 bg-gray-100 text-left">Age</div>
+        {comparisonsEntries.map(([id, emp]) => (
+          <div key={id} className="p-2 bg-white text-center">
+            {emp.employee_age}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,17 +1,51 @@
+import { useCallback } from "react";
 import { type IEmployeeData } from "../types/general";
 
 interface CardProps {
   employee: IEmployeeData;
+  onToggleCompare: (emp: IEmployeeData) => void;
+  isCompared: boolean;
+  canCompare: boolean;
 }
 
-const Card = ({ employee: emp }: CardProps) => {
+const Card = ({
+  employee: emp,
+  onToggleCompare,
+  isCompared,
+  canCompare,
+}: CardProps) => {
+  const handleToggleCompare = useCallback(() => {
+    onToggleCompare(emp);
+  }, [emp, onToggleCompare]);
+
   return (
-    <div className="shadow rounded-md overflow-hidden max-w-2xl">
-      <img
-        className="w-full h-auto aspect-square bg-gray-200"
-        alt={emp.employee_name}
-        src={emp.profile_image || "/images/256.png"}
-      />
+    <div className="group shadow rounded-md overflow-hidden max-w-2xl">
+      <div className="relative">
+        <img
+          className="w-full h-auto aspect-square bg-gray-200"
+          alt={emp.employee_name}
+          src={emp.profile_image || "/images/256.png"}
+        />
+        <div
+          className={`absolute top-0 left-0 w-full aspect-square bg-amber-300/75 transition opacity-0 flex items-center justify-center ${
+            isCompared
+              ? "opacity-100"
+              : canCompare
+              ? "group-hover:opacity-100"
+              : "opacity-0 pointer-events-none"
+          }`}
+        >
+          {isCompared ? (
+            <button onClick={handleToggleCompare} className="btn">
+              Remove
+            </button>
+          ) : (
+            <button onClick={handleToggleCompare} className="btn-outlined">
+              Compare
+            </button>
+          )}
+        </div>
+      </div>
       <div className="p-2">
         <div className="flex flex-row items-center">
           <h3 className="text-lg grow">{emp.employee_name}</h3>
